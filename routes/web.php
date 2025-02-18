14aca335599bf33b6ea881e33f4caf89ba3b6f26
nanaputra <agenaputra@gmail.com>
2025-02-18 11:49:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/about', function () {
-    return '2341720065 - Agna Putra Prawira';
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
 });
+
+// - Route Parameters
+/*Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});*/
+
+
+/*Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke -: " . $commentId;
+});/*
+
+
+/*Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '. $name;
+});*/
+
+// - Basic Routing 
+/* Route::get('/about', function () {
+    return '2341720065 - Agna Putra Prawira';
+});*/
  
+
 
 /* Route::get('/', function () {
     return 'Welcome';
